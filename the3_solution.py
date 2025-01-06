cb84d858c2002e44598d21956b8fd5c6ed96acad
said0 <saidgenckol22@gmail.com>
2025-01-06 08:55:53 +0300

    Integration of fucntions (1,2) and (3,4)

--- the3_solution.py
+++ the3_solution.py
@@ -2,6 +2,7 @@
 # Student2 Name: Batuhan Teberoğu Student2 ID: 2581056
 
 import cv2
+import os
 import numpy as np
 from matplotlib import pyplot as plt
 from skimage.feature import local_binary_pattern
@@ -18,7 +19,14 @@ def read_image(filename):
     return img
 #Write each image into its own folder
 def write_image(img, filename):
-    cv2.imwrite(output_folder+filename[0]+'/'+filename, img)
+    cv2.imwrite(output_folder+filename, img)
+#Create necessary folders
+def create_necessary_folders():
+    folders = ["image1", "image2", "image3", "image4", "image5", "image6"]
+    # Create the folders
+    for folder in folders:
+        folder_path = os.path.join(output_folder, folder)
+        os.makedirs(folder_path, exist_ok=True)
 
 
 #######################################
@@ -112,17 +120,15 @@ def morph(img, kernel, morph_type):
     o_img = cv2.morphologyEx(img, morph_type, kernel)
     return o_img
 
-# Gray-scale Morphology
+#Morphological operations
 def grayscale_morphology(image, operation, kernel_size=5):
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (kernel_size, kernel_size))
     if operation == 'dilation':
         return cv2.dilate(image, kernel, iterations=1)
     elif operation == 'erosion':
         return cv2.erode(image, kernel, iterations=1)
-    elif operation == 'opening':
-        return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
-    elif operation == 'closing':
-        return cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+    else:
+        return cv2.morphologyEx(image, operation, kernel)
 
 #Get Specific Channel of the Image
 def get_channel(image, channel):
@@ -161,8 +167,8 @@ def kmeans_rgb(image, k=2):
     #segmented = kmeans.labels_.reshape(image.shape[:2])
     return kmeans
 
-# KMeans using LBP Features
-def kmeans_lbp(image, k=2, radius=2, n_points=16):
+# KMeans using LBP Features (skimage)
+def kmeans_lbp_skimage(image, k=2, radius=2, n_points=16):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     red = get_channel(image, 'red')
     lbp= ELBP_skimage(gray, radius, n_points)
@@ -173,7 +179,20 @@ def kmeans_lbp(image, k=2, radius=2, n_points=16):
     kmeans = kmeans_clustering(lbp_flattened, k)
     segmented = kmeans.reshape(gray.shape)
     return segmented
+# Another way of implementing kmeans with extended lbp
+def kmeans_lbp(img, K, radius, neighbors):
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    lbp_features = ELBP(gray_img, radius, neighbors)
 
+    lbp_flattened = lbp_features.flatten().astype(np.float32).reshape(-1, 1)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+
+    _, label, center = cv2.kmeans(lbp_flattened, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+
+    center = np.uint8(center)
+    res = center[label.flatten()]
+    res = res.reshape((lbp_features.shape[0], lbp_features.shape[1]))
+    return res 
 # a general function to process and compare methods bluntly
 def process_image(image_path):
     image = cv2.imread(image_path)
@@ -187,7 +206,7 @@ def process_image(image_path):
     kmeans_rgb_result = kmeans_rgb(image, k=2)
 
     # Apply KMeans with LBP
-    kmeans_lbp_result = kmeans_lbp(image, k=2, radius=2, n_points=8)
+    kmeans_lbp_result = kmeans_lbp_skimage(image, k=2, radius=2, n_points=8)
 
     # Display results
     titles = ['Original', 'Grayscale', 'Red', 'Morph Opening', 'Morph Closing', 'KMeans RGB', 'KMeans LBP']
@@ -200,8 +219,94 @@ image_paths = [image_folder + str(i) + '.png' for i in range(1,6)]
 for path in image_paths:
     process_image(path)
 '''
+def image1_and_image2(img1, img2):
+    # Image 1
+    # Gray-scale Morphology
+    gray_1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    # Apply Morphological Gradient
+    grad_1 = grayscale_morphology(gray_1, cv2.MORPH_GRADIENT, 13)
+    # Postprocessing, thresholding
+    _, final_1 = cv2.threshold(grad_1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    write_image(final_1, 'image1/1_morph_gradient.png')
+
+    # KMeans Clustering using RGB Features
+    image1_kmeans = kmeans_clustering(img1, 2)
+    write_image(image1_kmeans, 'image1/1_kmeans_rgb.png')
+
+    # KMeans Clustering using Local Binary Pattern (LBP) Features
+    image1_kmeans_lbp = kmeans_lbp(img1, 2, 2, 16)
+    write_image(image1_kmeans_lbp, 'image1/1_kmeans_lbp.png')
+
+    # Image 2
+    # Gray-scale Morphology
+    gray_2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    # Apply Morphological Gradient
+    morph_2 = grayscale_morphology(gray_2,  cv2.MORPH_GRADIENT, 2)
+    # Postprocessing, thresholding
+    _, morph_2 = cv2.threshold(morph_2, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    write_image(morph_2, 'image2/2_morph_gradient.png')
+
+    # KMeans Clustering using RGB Features
+    image2_kmeans = kmeans_clustering(img2, 2)
+    write_image(image2_kmeans, 'image2/2_kmeans_rgb.png')
+
+    # KMeans Clustering using Local Binary Pattern (LBP) Features
+    image2_kmeans_lbp = kmeans_lbp(img2, 2, 2, 16)
+    write_image(image2_kmeans_lbp, 'image2/2_kmeans_lbp.png')
+
+def image3_and_image4(img3 ,img4):
+    # Image 3
+    # Gray-scale Morphology
+    image3_blackhat = grayscale_morphology(img3, cv2.MORPH_BLACKHAT, 11)
+    write_image(image3_blackhat, 'image3/3_blackhat.png')
+    image3_gradient = grayscale_morphology(img3, cv2.MORPH_GRADIENT, 3)
+    write_image(image3_gradient, 'image3/3_gradient.png')
+
+    # KMeans Clustering using RGB Features
+    image3_kmeans = kmeans_clustering(img3, 3)
+    write_image(image3_kmeans, 'image3/3_kmeans.png')
+
+    # KMeans Clustering using Local Binary Pattern (LBP) Features
+    image3_kmeans_lbp = kmeans_lbp(img3, 2, 2, 8)
+    write_image(image3_kmeans_lbp, 'image3/3_kmeans_lbp.png')
+
+    # Image 4
+    image4_blackhat = grayscale_morphology(img4, cv2.MORPH_BLACKHAT, 11)
+    write_image(image4_blackhat, 'image4/4_blackhat.png')
+    image4_gradient = grayscale_morphology(img4, cv2.MORPH_GRADIENT, 5)
+    write_image(image4_gradient, 'image4/4_gradient.png')
+
+    # KMeans Clustering using RGB Features
+    image4_kmeans = kmeans_clustering(img4, 2)
+    write_image(image4_kmeans, 'image4/4_kmeans.png')
+
+    # KMeans Clustering using Local Binary Pattern (LBP) Features
+    image4_kmeans_lbp = kmeans_lbp(img4, 2, 2, 16)
+    write_image(image4_kmeans_lbp, 'image4/4_kmeans_lbp.png')
+
+
 #MAIN
 if __name__ == '__main__':
+    
+    # Create folders inside output folder to make it organized
+    create_necessary_folders()
+    # Read images
+    img_1 = read_image('1.png')
+    img_2 = read_image('2.png')
+    img_3 = read_image('3.png')
+    img_4 = read_image('4.png')
+    img_5 = read_image('5.png')
+    img_6 = read_image('6.png')
+
+    # Images 1 and 2
+    #image1_and_image2(img_1, img_2)
+
+    # Images 3 and 4
+    image3_and_image4(img_3, img_4)
+
+    # Images 5 and 6
+    #image5_and_image6(img_5, img_6)
+    '''
     #Image5
     img_5 = read_image('5.png')
     image = img_5
@@ -212,6 +317,7 @@ if __name__ == '__main__':
     write_image(kmeans_rgb_result, '5_kmeans_rgb.png')
     # Apply KMeans with LBP
     
-    kmeans_lbp_result = kmeans_lbp(image, k=2, radius=2, n_points=16)
+    kmeans_lbp_result = kmeans_lbp_skimage(image, k=2, radius=2, n_points=16)
     write_image(kmeans_lbp_result, '5_kmeans_lbp.png')
     #Image6
+    '''
